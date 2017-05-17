3db20c7e91eacbed72b85b79981c3abb58c611e3
code-n-roll <romankaranchuk@gmail.com>
2017-05-17 03:19:32 +0300

    classwork#6(complete) did extra task: stroke width problem in custom view paint for eyes, mouth i.e. simple solution is to use getResources().getDisplayMetrics().density without multiplication by constant added module task6 for homework#6

--- task5/src/main/java/com/androidlab2017/epam/SmileyView.java
+++ task5/src/main/java/com/androidlab2017/epam/SmileyView.java
@@ -1,11 +1,17 @@
 package com.androidlab2017.epam;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GestureDetectorCompat;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -13,13 +19,28 @@ import android.view.View;
  */
 
 public class SmileyView extends View {
-    private Paint mCirclePaint;
-    private Paint mEyeAndMouthPaint;
+    private Paint mFacePaint;
+    private Paint mEyesPaint;
+    private Paint mMouthPaint;
 
+    private int mEyesColor;
+    private int mMouthColor;
+    private int mFaceColor;
+    private int mEmotion;
     private float mCenterX;
     private float mCenterY;
     private float mRadius;
-    private RectF mArcBounds = new RectF();
+    private float mEyeRadius;
+    private float mEyeOffsetY;
+    private float mEyeOffsetX;
+    private RectF mArcBoundsFunny = new RectF();
+    private RectF mArcBoundsSad = new RectF();
+    private final static int FUNNY = 0,
+                             SAD = 1;
+    private final static String SUPER_STATE_TAG = "superState",
+                                EMOTION_TAG = "emotion";
+    private GestureDetectorCompat mGestureDetector;
+
 
     public SmileyView(Context context) {
         this(context, null);
@@ -31,19 +52,79 @@ public class SmileyView extends View {
 
     public SmileyView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initAttrs(context, attrs);
         initPaints();
+        initGestureDetector(context);
+    }
+
+    private void initGestureDetector(Context context){
+        mGestureDetector = new GestureDetectorCompat(context, mGestureListener);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        mGestureDetector.onTouchEvent(event);
+        return true;
+    }
+
+
+    private GestureDetector.OnGestureListener mGestureListener =
+            new GestureDetector.SimpleOnGestureListener(){
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            changeEmotion();
+            invalidate();
+            requestLayout();
+            return super.onSingleTapUp(e);
+        }
+    };
+
+    private void changeEmotion(){
+        switch (mEmotion){
+            case FUNNY:
+                mEmotion = SAD;
+                break;
+            case SAD:
+                mEmotion = FUNNY;
+                break;
+        }
+    }
+
+    private void initAttrs(Context context, AttributeSet attrs){
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.SmileyView,
+                0,0
+        );
+
+        try{
+            mEyesColor = a.getColor(R.styleable.SmileyView_eyesColor,
+                    ContextCompat.getColor(context, android.R.color.black));
+            mMouthColor = a.getColor(R.styleable.SmileyView_mouthColor,
+                    ContextCompat.getColor(context, android.R.color.black));
+            mFaceColor = a.getColor(R.styleable.SmileyView_faceColor,
+                    ContextCompat.getColor(context, android.R.color.white));
+            mEmotion = a.getInt(R.styleable.SmileyView_emotion, 0);
+        } finally {
+            a.recycle();
+        }
     }
 
     private void initPaints() {
-        mCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mCirclePaint.setStyle(Paint.Style.FILL);
-        mCirclePaint.setColor(Color.YELLOW);
-        mEyeAndMouthPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mEyeAndMouthPaint.setStyle(Paint.Style.STROKE);
-        mEyeAndMouthPaint.setStrokeWidth(16 * getResources().getDisplayMetrics().density);
-        mEyeAndMouthPaint.setStrokeCap(Paint.Cap.ROUND);
-        mEyeAndMouthPaint.setColor(Color.BLACK);
+        mFacePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mFacePaint.setStyle(Paint.Style.FILL);
+        mFacePaint.setColor(mFaceColor);
+
+        mEyesPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mEyesPaint.setStyle(Paint.Style.STROKE);
+        mEyesPaint.setStrokeWidth(getResources().getDisplayMetrics().density);
+        mEyesPaint.setStrokeCap(Paint.Cap.ROUND);
+        mEyesPaint.setColor(mEyesColor);
+
+        mMouthPaint = new Paint(mEyesPaint);
+        mMouthPaint.setColor(mMouthColor);
     }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -63,18 +144,61 @@ public class SmileyView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+        initSizes();
         // draw face
-        canvas.drawCircle(mCenterX, mCenterY, mRadius, mCirclePaint);
+        canvas.drawCircle(mCenterX, mCenterY, mRadius, mFacePaint);
         // draw eyes
-        float eyeRadius = mRadius / 5f;
-        float eyeOffsetX = mRadius / 3f;
-        float eyeOffsetY = mRadius / 3f;
-        canvas.drawCircle(mCenterX - eyeOffsetX, mCenterY - eyeOffsetY, eyeRadius, mEyeAndMouthPaint);
-        canvas.drawCircle(mCenterX + eyeOffsetX, mCenterY - eyeOffsetY, eyeRadius, mEyeAndMouthPaint);
+        canvas.drawCircle(mCenterX - mEyeOffsetX, mCenterY - mEyeOffsetY, mEyeRadius, mEyesPaint);
+        canvas.drawCircle(mCenterX + mEyeOffsetX, mCenterY - mEyeOffsetY, mEyeRadius, mEyesPaint);
         // draw mouth
-        float mouthInset = mRadius /3f;
+        chooseEmotion(canvas);
+    }
 
-        mArcBounds.set(mouthInset, mRadius*1.5f - mouthInset, mRadius * 2 - mouthInset, mRadius * 3 - mouthInset);
-        canvas.drawArc(mArcBounds, 225f, 90f, false, mEyeAndMouthPaint);
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(SUPER_STATE_TAG, super.onSaveInstanceState());
+        bundle.putInt(EMOTION_TAG, mEmotion);
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state instanceof Bundle){
+            Bundle bundle = (Bundle) state;
+            mEmotion = bundle.getInt(EMOTION_TAG);
+            state = bundle.getParcelable(SUPER_STATE_TAG);
+        }
+        super.onRestoreInstanceState(state);
+    }
+
+    private void chooseEmotion(Canvas canvas){
+        switch (mEmotion){
+            case FUNNY:
+                canvas.drawArc(mArcBoundsFunny, 45f, 90f, false, mMouthPaint);
+                break;
+            case SAD:
+                canvas.drawArc(mArcBoundsSad, 225f, 90f, false, mMouthPaint);
+                break;
+        }
+    }
+
+    private void initSizes(){
+        //eyes params
+        mEyeRadius = mRadius / 5f;
+        mEyeOffsetY = mRadius / 3f;
+        mEyeOffsetX = mRadius / 3f;
+        //mouth params
+        float mMouthInset = mRadius / 3f;
+        switch (mEmotion){
+            case FUNNY:
+                mArcBoundsFunny.set(mMouthInset, mMouthInset,
+                        mRadius * 2 - mMouthInset, mRadius * 2 - mMouthInset);
+                break;
+            case SAD:
+                mArcBoundsSad.set(mMouthInset, mRadius * 1.5f - mMouthInset,
+                        mRadius * 2 - mMouthInset, mRadius * 3 - mMouthInset);
+                break;
+        }
     }
 }
---
+++ task6/src/main/java/com/androidlab2017/epam/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidlab2017.epam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
