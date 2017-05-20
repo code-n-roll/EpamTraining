1a145353e9978c8d526cd7b01a872102b0564c15
code-n-roll <romankaranchuk@gmail.com>
2017-05-20 21:31:20 +0300

    init commit

--- app/src/main/java/com/androidlab2017/epam/tasks/task3/OnChangeFragmentListener.java
+++
@@ -1,9 +0,0 @@
-package com.androidlab2017.epam.tasks.task3;
-
-/**
- * Created by roman on 10.5.17.
- */
-
-public interface OnChangeFragmentListener {
-    void onChangeFragment();
-}
--- task6/src/main/java/com/androidlab2017/epam/Item.java
+++
@@ -1,8 +0,0 @@
-package com.androidlab2017.epam;
-
-/**
- * Created by roman on 19.5.17.
- */
-
-public class Item {
-}
